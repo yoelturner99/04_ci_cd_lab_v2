e3a197845666bbd6f755014315b413b6302290e2
yoelturner99 <yoelturner99@gmail.com>
2023-01-12 14:45:50 +0100

    Removed src in import of user_functions

--- test_user_functions.py
+++ test_user_functions.py
@@ -1,6 +1,6 @@
 import io
 import pytest
-from src.user_functions import *
+from user_functions import *
 
 def test_email_with_user_input_no_at_sign(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
