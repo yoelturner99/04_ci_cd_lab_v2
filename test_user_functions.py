b27513f1e36bf641c4e6d5d3493befa23a7ac170
yoelturner99 <yoelturner99@gmail.com>
2023-01-12 14:40:13 +0100

    Initial commit

---
+++ test_user_functions.py
@@ -0,0 +1,17 @@
+import io
+import pytest
+from src.user_functions import *
+
+def test_email_with_user_input_no_at_sign(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
+    assert get_email_from_input() is None
+
+# Check for incorrect email format
+def test_email_with_user_input_regex_no_at_sign(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
+    assert get_email_from_input_regex() is None
+
+# Check for incorrect email format
+def test_email_with_user_input_regex_valid(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('yoel_turner99@adaltas.com'))
+    assert get_email_from_input_regex() == 'yoel_turner99@adaltas.com'
---
+++ user_functions.py
@@ -0,0 +1,27 @@
+def get_user_name_from_input():
+    """ Not empty string. No spaces. """
+    return input("Create your user name: ")
+
+def get_password_from_input():
+    """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
+    return input("Create your password: ")
+
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Write down your email: ")
+
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.')
+    else:
+        return email
+
+
+def get_email_from_input_regex():  
+    import re
+    regex = '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$'
+    email = input("Write down your email: ")
+
+    if(re.search(regex,email)):  
+        return email 
+    else:  
+        print("Invalid Email")
